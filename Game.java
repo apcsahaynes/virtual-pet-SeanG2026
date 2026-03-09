318dd2e60f74804c4aac89dab0489cc341747282
SeanG2026 <sean.goodwin@jpschools.org>
2026-03-09 11:12:02 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,3 +1,27 @@
-public class Game {
+public class Game{
+    private String name;
+    private int happinessIncrease;
+    private int weightDecrease;
     
+    public Game(String name, int happinessIncrease, int weightDecrease){
+        this.name = name;
+        this.happinessIncrease = happinessIncrease;
+        this.weightDecrease = weightDecrease;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public int getHappinessIncrease(){
+        return happinessIncrease;
+    }
+    
+    public int getWeightDecrease(){
+        return weightDecrease;
+    }
+    
+    public boolean isWinner(){
+        return Math.random() < 0.5;
+    }
 }
